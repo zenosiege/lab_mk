5053d5636d3d17c46115f31f0ce5c89ff14f73a6
Pantileev Danila <neeta7love@gmail.com>
2024-10-05 12:47:43 +0300

    Make needed functions for src.ino in led.h

--- src/led.h
+++ src/led.h
@@ -1,15 +1,15 @@
 void builtin_led_init() {
-
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void builtin_led_on() {
-
+    digitalWrite(LED_BUILTIN, HIGH);  
 }
 
 void builtin_led_off() {
-
+    digitalWrite(LED_BUILTIN, LOW); 
 }
 
-void delay_s() {
-
+void delay_s(unsigned int s) {
+    delay(s * 1000); 
 }
