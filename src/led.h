c87b972b0daeaca9c5c09d345e9ac395fc9ff4b3
Pantileev Danila <neeta7love@gmail.com>
2024-10-09 18:41:30 +0300

    Add RGB_LED and color change

--- src/led.h
+++ src/led.h
@@ -1,3 +1,4 @@
+
 /* Outdated code
 namespace Builtin_led {
 
@@ -28,6 +29,86 @@ class LED {
         void off() { digitalWrite(pin, LOW); }
 
     private:
+        int pin;
+
+};
+
+class RGB_LED {
+    public:
+
+        RGB_LED(int a_pin, int r_pin, int g_pin, int b_pin): pin{a_pin}, rpin{r_pin}, gpin{g_pin}, bpin{b_pin} {};
+
+        void init() { pinMode(pin, OUTPUT); pinMode(rpin, OUTPUT); pinMode(gpin, OUTPUT); pinMode(bpin, OUTPUT);}
+
+        void on() { digitalWrite(pin, HIGH); }
+
+        void redLightOn() { digitalWrite(rpin, LOW); digitalWrite(gpin, HIGH); digitalWrite(bpin, HIGH);}
+
+        void off() { digitalWrite(pin, LOW); }
+
+        void redLightOff() { digitalWrite(rpin, HIGH); digitalWrite(gpin, LOW); digitalWrite(bpin, LOW);}
+
+        void clrRGBon(char color) {
+            switch (color) {
+                case 'r':
+                    digitalWrite(rpin, LOW); 
+                    digitalWrite(gpin, HIGH); 
+                    digitalWrite(bpin, HIGH);
+                    break;
+                case 'g':
+                    digitalWrite(rpin, HIGH); 
+                    digitalWrite(gpin, LOW); 
+                    digitalWrite(bpin, HIGH);
+                    break;
+                case 'b':
+                    digitalWrite(rpin, HIGH); 
+                    digitalWrite(gpin, HIGH); 
+                    digitalWrite(bpin, LOW);
+                    break;
+                case 'w':
+                    digitalWrite(rpin, LOW); 
+                    digitalWrite(gpin, LOW); 
+                    digitalWrite(bpin, LOW);
+                    break;
+                default:
+                    Serial.print('che durak tak delat?');
+                    break;
+            }
+        }
+
+        void clrRGBoff(char color) {
+            switch (color) {
+                case 'r':
+                    digitalWrite(rpin, HIGH); 
+                    digitalWrite(gpin, LOW); 
+                    digitalWrite(bpin, LOW);
+                    break;
+                case 'g':
+                    digitalWrite(rpin, LOW); 
+                    digitalWrite(gpin, HIGH); 
+                    digitalWrite(bpin, LOW);
+                    break;
+                case 'b':
+                    digitalWrite(rpin, LOW); 
+                    digitalWrite(gpin, LOW); 
+                    digitalWrite(bpin, HIGH);
+                    break;
+                case 'w':
+                    digitalWrite(rpin, HIGH); 
+                    digitalWrite(gpin, HIGH); 
+                    digitalWrite(bpin, HIGH);
+                    break;
+                default:
+                    Serial.print('che durak tak delat?');
+                    break;
+            }
+        }
+
+
+    private:
+        int rpin;
+        int gpin;
+        int bpin;
         int pin;
 
 };
--- src/src.ino
+++ src/src.ino
@@ -4,7 +4,7 @@ constexpr unsigned int PERIOD{2};
 constexpr unsigned int HALF_PERIOD{PERIOD / 2};
 
 LED builtin_led(13);
-LED another_led(8);
+RGB_LED another_led(12, 11, 9, 10);
 
 void setup() {
     builtin_led.init();
@@ -14,9 +14,11 @@ void setup() {
 void loop() {
     builtin_led.on();
     another_led.on();
+    another_led.clrRGBon('w');
     delay_s(HALF_PERIOD);
     builtin_led.off();
     another_led.off();
+    another_led.clrRGBoff('w');
     delay_s(HALF_PERIOD);
     
     Serial.print(":3 ");
