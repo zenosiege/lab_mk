56d890fc67184874e4e2a5348e93e32e3d23e177
Pantileev Danila <neeta7love@gmail.com>
2024-10-09 17:19:07 +0300

    Add another LED object to src

--- src/src.ino
+++ src/src.ino
@@ -4,6 +4,7 @@ constexpr unsigned int PERIOD{2};
 constexpr unsigned int HALF_PERIOD{PERIOD / 2};
 
 LED builtin_led(13);
+LED another_led(8);
 
 void setup() {
     builtin_led.init();
@@ -12,8 +13,10 @@ void setup() {
 
 void loop() {
     builtin_led.on();
+    another_led.on();
     delay_s(HALF_PERIOD);
     builtin_led.off();
+    another_led.off();
     delay_s(HALF_PERIOD);
     
     Serial.print(":3 ");
