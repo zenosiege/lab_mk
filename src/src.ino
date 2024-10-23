2b6715e41472b4373837e95488984993f3d4ae85
Pantileev Danila <neeta7love@gmail.com>
2024-10-23 17:19:11 +0300

    Reformat code in src.ino using templates

--- src/src.ino
+++ src/src.ino
@@ -1,25 +1,21 @@
-#include "led.h"
+// #include "led.h"
+#include "led_templ.h"
 
 constexpr unsigned int PERIOD{2};
 constexpr unsigned int HALF_PERIOD{PERIOD / 2};
 
-LED builtin_led(13);
-RGB_LED another_led(12, 11, 9, 10);
+using Builtin_LED = LED<13>;
+
 
 void setup() {
-    builtin_led.init();
+    Builtin_LED::init();
     Serial.begin(115200);
 }
 
 void loop() {
-    builtin_led.on();
-    another_led.on();
-    another_led.clrRGBon('w');
+    Builtin_LED::on();
     delay_s(HALF_PERIOD);
-    builtin_led.off();
-    another_led.off();
-    another_led.clrRGBoff('w');
+    Builtin_LED::off();
     delay_s(HALF_PERIOD);
-    
     Serial.print(":3 ");
 }
