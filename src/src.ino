ecc57e8f501bbca6ce4981dcf4ecdb013c4deb35
Pantileev Danila <neeta7love@gmail.com>
2024-11-20 18:32:38 +0300

    Add basic game mechanics

--- src/src.ino
+++ src/src.ino
@@ -5,6 +5,8 @@
 constexpr unsigned int PERIOD{2};
 constexpr unsigned int HALF_PERIOD{PERIOD / 2};
 
+int howManyClicked = 0; // количество кликов
+int cheatDetect = 0; // защита от "читерства", от бесконечно приложенной руки
 
 using Builtin_LED = LED<13>;
 using RGBT = RGB_LED<12, 11, 9, 10, Cathode_driven>;
@@ -15,6 +17,7 @@ void setup() {
     RGBT::init();
 
     Serial.begin(115200);
+    Serial.println("WELCOME TO ARDUINO COMBAT!");
 }
 
 void loop() {
@@ -23,11 +26,19 @@ void loop() {
     int PhotoValue = PhotoSensor1::Read();
 
     // RGBT::staticRGBlight();
-    Serial.println(PhotoValue);
+    
     if (PhotoValue > 400) {
+        cheatDetect = 0;
         RGBT::greenLight();
     }
-    else {
+    else if ((PhotoValue < 400) && cheatDetect == 0) {
+        // если перекрываешь сенсор рукой - плюс клик
+        howManyClicked += 1;
+        
+        // нельзя бесконечно фармить
+        cheatDetect = 1;
+
+        Serial.println(howManyClicked);
         RGBT::redLight();
     }
     // исправить взаимодействие светодиода с фотодатчиком
